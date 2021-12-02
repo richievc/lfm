b42286bc3d3d07fbb40087f8bb46147215fb74f2
richievc <richievc@gmail.com>
2021-12-01 20:10:07 -0500

    install

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -146,13 +146,12 @@
                     </div>
                     <div class="col-lg-3 col-md-6 m-b-30">
                         <h5 class="m-b-20 text-white">Phone</h5>
-                        <p>Reception : +999 999 9999
-                            <br/>Office : +207 235 7890</p>
+                        <p>Reception : 999-999-9999</p>
                     </div>
                     <div class="col-lg-3 col-md-6 m-b-30">
                         <h5 class="m-b-20 text-white">Email</h5>
-                        <p>Office : <a href="#" class="link text-white">info@wrappixel.com</a>
-                            <br/>Site : <a href="#" class="link text-white">wrapkit@wrappixel.com</a></p>
+                        <p>Office : <a href="#" class="link text-white">info@LiquefireMedia.com</a>
+                            <br/>Site : <a href="#" class="link text-white">sales@LiquefireMediad.com</a></p>
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <h5 class="m-b-20 text-white">Social</h5>
@@ -169,7 +168,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="d-flex font-14">
-                                <div class="m-t-10 m-b-10 copyright">All Rights Reserved by WrapPixel.</div>
+                                <div class="m-t-10 m-b-10 copyright">All Rights Reserved by LiquefireMediad.</div>
                                 <div class="links ml-auto m-t-10 m-b-10">
                                     <a href="#" class="p-10 p-l-0">Terms of Use</a>
                                     <a href="#" class="p-10">Legal Disclaimer</a>
